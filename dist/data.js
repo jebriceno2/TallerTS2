69cfc66eca004333dfa68e758f6e47c6500f5495
Juan <je.briceno2@uniandes.edu.co>
2025-04-06 18:31:06 -0500

    Cambios de series

--- dist/data.js
+++ dist/data.js
@@ -1,10 +1,10 @@
 // src/data.ts
 import { Serie } from "./serie.js";
 export const dataSeries = [
-    new Serie(1, "Breaking Bad", "AMC", 5, "https://www.amc.com/shows/breaking-bad"),
-    new Serie(2, "Orange Is the New Black", "Netflix", 6, "https://www.netflix.com/title/70242311"),
-    new Serie(3, "Game of Thrones", "HBO", 8, "https://www.hbo.com/game-of-thrones"),
-    new Serie(4, "The Big Bang Theory", "CBS", 12, "https://www.cbs.com/shows/big_bang_theory/"),
-    new Serie(5, "Sherlock", "BBC", 4, "https://www.bbc.co.uk/programmes/b018ttws"),
-    new Serie(6, "Stranger Things", "Netflix", 4, "https://www.netflix.com/title/80057281")
+    new Serie(1, "Sousou no Frieren", "Crunchyroll", 2, "https://www.crunchyroll.com/es/series/GG5H5XQX4/frieren-beyond-journeys-end?srsltid=AfmBOoovKvxb5BuhZGD1b8R2uwRv8FhOg1UQxWqn-8rmKrNgROCbEAtV"),
+    new Serie(2, "Monogatari: Segunda Temporada", "Crunchyroll", 6, "https://www.crunchyroll.com/es/series/GYWEMJ1GY/monogatari-second-season"),
+    new Serie(3, "Kaguya-Sama Love is war", "Crunchyroll", 3, "https://www.crunchyroll.com/es/series/GRJ0J828Y/kaguya-sama-love-is-war?srsltid=AfmBOopqVj3G4xkss4G_Epvx-2ce191L-RHNUYmXiteDQTaUbv-4Be76"),
+    new Serie(4, "Jojo's Bizarre Adventure", "Crunchyroll", 6, "https://www.crunchyroll.com/es/series/GYP8DP1MY/jojos-bizarre-adventure"),
+    new Serie(5, "Bleach", "Disney+", 12, "https://www.disneyplus.com/en-gb/browse/entity-c649c634-7f8d-48a9-ba72-48bd4b9c8419"),
+    new Serie(6, "Solo Leveling", "Crunchyroll", 2, "https://www.crunchyroll.com/es/series/GDKHZEJ0K/solo-leveling")
 ];
